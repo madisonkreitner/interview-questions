755992892c26d2692c3c4e4aebd6e248416704a8
mbradley-github <76399993+mbradley-github@users.noreply.github.com>
2022-02-23 22:58:44 -0700

    saving changes

--- binary-search/Solution.py
+++ binary-search/Solution.py
@@ -1,6 +1,20 @@
+# Given an array of integers nums which is sorted in ascending order, 
+# and an integer target, write a function to search target in nums. 
+# If target exists, then return its index. Otherwise, return -1.
+# You must write an algorithm with O(log n) runtime complexity.
+
 class Solution:
     def binarySearch(self, nums, target):
+        # O(n) time complexity
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         return i
+        # return -1
+
+        #sorted in ascending order, so we can use a binary search
+        #first we need a pointer
         pass
+        
 
 class TestCase:
     def test(self, solution, nums, target):
