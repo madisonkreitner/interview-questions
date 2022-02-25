5054c290f18a587e36b6804d68fdaaa6f5fbbcf3
mbradley-github <76399993+mbradley-github@users.noreply.github.com>
2022-02-24 21:03:11 -0700

    palidrome number solution

---
+++ palindrome-number/Solution.py
@@ -0,0 +1,37 @@
+class Solution:
+    def palindromeNumber(self, x):
+        val = str(x)
+
+        if len(val) == 0:
+            return False
+        elif len(val) == 1:
+            return True
+        
+        left = None
+        right = None
+
+        if len(val) % 2 != 0:
+            left = len(val) // 2 - 1
+            right = len(val) // 2 + 1
+        else:
+            left = len(val) // 2 - 1
+            right = len(val) // 2
+
+        while left >= 0:
+            if val[left] != val[right]:
+                return False
+            left -= 1
+            right += 1
+
+        return True
+
+class TestCase:
+    def test(self, solution, n):
+        print(solution.palindromeNumber(n))
+
+SolutionTester = TestCase()
+mySolution = Solution()
+
+SolutionTester.test(mySolution, 121)
+SolutionTester.test(mySolution, -121)
+SolutionTester.test(mySolution, 10)
