7c99c9ba324e18a0b2a60f69ce7223850f2e3d3e
mbradley-github <76399993+mbradley-github@users.noreply.github.com>
2022-02-28 18:19:49 -0700

    two sum trivial solution

--- two-sum/Solution.py
+++ two-sum/Solution.py
@@ -1,20 +1,14 @@
 class Solution:
     def twoSum(self, nums, target):
-        indeces = [0,0]
-
         for i in range(len(nums)):
-            slice = nums[i+1:]
-            for j in range(len(slice)):
-                if nums[i] + slice[j] == target:
-                    indeces = [i,j+1]
-                    values = [nums[i], slice[j]]
-                    print(indeces, values, target)
-                    return
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return (i, j)
         
 
 class TestCase:
     def test(self, solution, nums, target):
-        solution.twoSum(nums, target)
+        print(solution.twoSum(nums, target))
 
 SolutionTester = TestCase()
 mySolution = Solution()
